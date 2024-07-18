a8fadd9cf70cbd806e8f586d8f458a2a4457b664
Divyanshi Kansal <113118148+Divyanshiikansall@users.noreply.github.com>
2024-07-18 15:37:02 +0530

    Update and rename prog11.py to converting list into tables.py

--- converting list into tables.py
+++ converting list into tables.py
@@ -1,7 +1,7 @@
 #program 11 anudip foundation
 # Example of a list representing tabular data
 from tabulate import tabulate
-student = [
+student = [                       #list is entered manually by the user. it is a nested list.
     ["stdid","stdname","standard","age","Hindi","English","Maths","Science","Computer","Total"],
     ["std101","Ashish ","10th",15,67,89,87,89,90,422],
     ["std102","Abhishek ","10th",14,85,45,48,90,45,313],
@@ -14,20 +14,20 @@ student = [
     ["std109","Kamlesh","10th",15,45,56,78,99,67,345],
     ["std110","Rohan","10th",15,34,12,24,45,56,171]
 ]
-def print_table(student): 
+def print_table(student): #function is used to prinst the list given by user in table format
      for row in student:
         print("| {:<10} | {:<10} | {:<10} | {:<5} | {:<5} | {:<10} | {:<5} | {:<10} | {:<10} | {:<5} |".format(*row))
-def query_1(student):
+def query_1(student):  #function for query 1
     print("\nQuery 1: Names of students whose marks in English are greater than 50")
     for row in student[1:]:  
         if row[6] > 50:  
             print(row[1])
-def query_2(student):
+def query_2(student):  #function for query 2
     print("\nQuery 2: Names and ages of the top four scorers in Maths")
     sorted_table = sorted(student[1:],  reverse=True)  
     for row in sorted_table[:4]:
         print(f"{row[1]:<10} | Age: {row[3]:<7} |")
-def query_3(student):
+def query_3(student):  #function for query 3
     print("\nQuery 3: Names,id nd ages student who are bottom three scorer in computer")
     sorted_table = sorted(student[1:],  reverse=True)  
     for row in sorted_table[:3]:
