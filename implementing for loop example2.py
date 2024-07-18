b3083fd2e6356770d72405c97eda21e307077194
Divyanshi Kansal <113118148+Divyanshiikansall@users.noreply.github.com>
2024-07-18 15:26:22 +0530

    Update and rename prog8.py to implementing for loop example2.py

--- implementing for loop example2.py
+++ implementing for loop example2.py
@@ -1,10 +1,11 @@
-#program 8 anudip foundation
-
-total_num=0
+#program 9 anudip foundation
+# for loop example 2
+total_num=0 # used for counting total number which satisfy given condition
 print("total numbers between 100 to 1000 which are even and divisble be 3:")
-for i in range(100,1000):
+for i in range(100,1000): # 100 is included and 1000 is excluded , step value is 1.
+    # forward indexing as positive numbers are used
     if(i%2==0 and i%3==0):
         total_num+=1;
-        print(i," , ",end="")
+        print(i," , ",end="") # to print all the numbers in the same line
 print("\n----------------------------------------------------------------------------------------------------------------------------")
 print("\n total posible number satisfying the given condition:",total_num)
